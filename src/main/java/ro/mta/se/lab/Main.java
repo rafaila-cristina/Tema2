113ed355ff1b452cb3ace39d61711a90bd281bbd
rafaila-cristina <rafaila.cristina@gmail.com>
2021-01-11 19:34:40 +0200

    Initializare Proiect
    Am creat un proiect maven. Am adaugat in fisierul pom.xml dependintele necesare rularii proiectului si plugin pentru a rula cu javafx. Am creat package-ul conform cerintelor.
    De asemenea am creat fisierul Cities.txt in care se afla cele 10 tari cu cate 2 orase fiecare.
    Am creat si fisierul .gitignore cu ajutorul site-ului gitignore.io.
    
    Signed-off-by: rafaila-cristina <rafaila.cristina@gmail.com>

---
+++ src/main/java/ro/mta/se/lab/Main.java
@@ -0,0 +1,31 @@
+package ro.mta.se.lab;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.Stage;
+
+import javax.imageio.IIOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch (IIOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+
+
+    public static void main(String[] args) {
+             launch(args);
+
+    }
+}
